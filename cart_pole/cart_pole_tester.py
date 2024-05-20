15d53a5a06a05838eecba3f7db09eca9199c2add
filomba01 <f.balzarini@an4a.com>
2024-05-20 18:01:41 +0200

    report revision, plot fixing

--- cart_pole/cart_pole_tester.py
+++ cart_pole/cart_pole_tester.py
@@ -4,7 +4,6 @@ import sys
 import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 
 N_TESTS = 10
@@ -45,36 +44,36 @@ if len(sys.argv) > 2:
   qTable, bins, obsSpaceSize  = load_q_table(qTablePath)
 else:
   qTable, bins, obsSpaceSize  = load_q_table('RL/data/qTable_1_20000_10000_-375.npy')
-  population = np.load('data/GA_v2_population_100_fitness_0.005_fitness_2_10.npy',allow_pickle=True)
+  population = np.load('data/GA_v2_population_100_fitness_0.005_fitness_2_10_filippo.npy',allow_pickle=True)
 
 
 def test(qTable, env,  numEpisodes=1,  seed = SEED[0], GA = False, bins = bins, obsSpaceSize = obsSpaceSize):
     # Test the trained q-table with the environment
     best_reward = 0
     seed = int(seed)
-    for episode in range(numEpisodes):
-        state = env.reset(seed=seed)
-        done = False
-        steps = 0
-        discrete_state = get_discrete_state(env.observation_space.high, bins, obsSpaceSize)
-        frames = []
-        while not done and steps < 500:
-            frames.append(env.render())
-            if GA:
-                action = int(qTable[discrete_state])
-            else:
-                action = np.argmax(qTable[discrete_state])
-            state, reward, done, _, info = env.step(action)
-            discrete_state = get_discrete_state(state, bins, obsSpaceSize)
-            steps += reward
 
-        # save the frames in frames folder
-        #for i, frame in enumerate(frames):
-        #    plt.imsave(f'frames/RL_{episode}_{i}.png', frame)
-        
-        print(f'Episode {episode} reward: {steps}')
-        if steps > best_reward:
-             best_reward = steps
+    state = env.reset(seed=seed)
+    done = False
+    steps = 0
+    discrete_state = get_discrete_state(env.observation_space.high, bins, obsSpaceSize)
+    frames = []
+    while not done and steps < 500:
+        frames.append(env.render())
+        if GA:
+            action = int(qTable[discrete_state])
+        else:
+            action = np.argmax(qTable[discrete_state])
+        state, reward, done, _, info = env.step(action)
+        discrete_state = get_discrete_state(state, bins, obsSpaceSize)
+        steps += reward
+
+    # save the frames in frames folder
+    #for i, frame in enumerate(frames):
+    #    plt.imsave(f'frames/RL_{episode}_{i}.png', frame)
+    
+    #print(f'reward: {steps}')
+    if steps > best_reward:
+            best_reward = steps
 
     return best_reward
 
@@ -85,7 +84,7 @@ def plot_table_diff(qTable, best_ind):
     plt.imshow(diff, cmap='hot', interpolation='nearest')
     plt.show()
     # save the plot
-    plt.imsave('plots/diff.png',diff, cmap='hot')
+    plt.imsave('plots/diff_test.png',diff, cmap='hot')
     # include axis in the saved plot
 
 def plot_results(agent_results, best_ind_rew, xlabel='Test Number', ylabel='Reward', ga_color='blue', rl_color='red'):
@@ -97,16 +96,16 @@ def plot_results(agent_results, best_ind_rew, xlabel='Test Number', ylabel='Rewa
     ga_std_dev = np.std(best_ind_rew)
 
     
-    plt.fill_between(range(N_TESTS+1), agent_mean - agent_std_dev, agent_mean + agent_std_dev, color=rl_color, alpha=0.05)
-    plt.fill_between(range(N_TESTS+1), ga_mean - ga_std_dev, ga_mean + ga_std_dev, color=ga_color, alpha=0.05)
+    plt.fill_between(range(1,N_TESTS+1), agent_mean - agent_std_dev, agent_mean + agent_std_dev, color=rl_color, alpha=0.05)
+    plt.fill_between(range(1,N_TESTS+1), ga_mean - ga_std_dev, ga_mean + ga_std_dev, color=ga_color, alpha=0.05)
 
-    plt.plot(np.arange(N_TESTS+1)[1:], best_ind_rew, label='GA', color=ga_color)
-    plt.plot(np.arange(N_TESTS+1)[1:], agent_results, label='RL', color=rl_color)
-
+    plt.plot(np.arange(1,N_TESTS+1), best_ind_rew, label='GA', color=ga_color)
+    plt.plot(np.arange(1,N_TESTS+1), agent_results, label='RL', color=rl_color)
+    plt.ylim(0,510)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.legend()
-    plt.savefig('plots/RL_vs_GA.png')
+    plt.legend(fontsize='large')
+    plt.savefig('plots/RL_vs_GA_test.png')
     plt.show()
 
 best_ind_rew = np.zeros(N_TESTS)
@@ -120,7 +119,7 @@ for i in range(N_TESTS):
     best_ind = population[0]
     print('### GA Testing ###')
     for j in range(len(population)):
-        print(f'Testing individual {j}')
+        #print(f'Testing individual {j}')
         reward = test(population[j],env, GA = True, seed = SEED[i]) 
         if reward > best_ind_rew[i]:
             best_ind_rew[i] = reward
